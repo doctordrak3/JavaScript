6c9b97593faf1397bac04553a18b3f05170140fb
doctordrak3 <dabshepard@gmail.com>
2024-04-03 15:36:31 -0600

    Send Changes

--- Project2/Excercise1/app.js
+++ Project2/Excercise1/app.js
@@ -11,3 +11,11 @@ let encryptedMessage = ""; // String
 let decryptedMessage = ""; // String
 let isPartySafe = false; // Boolean
 //Question 3
+let partyLocation = "GARDEN"; // As the message might change.
+const shiftValue = 3; // Since the shift value remains constant throughout.
+let encryptedMessage = ""; // This will change when we encode the message with the original party location.
+let decryptedMessage = ""; // This will change when we decode the encrypted message to reveal the party location.
+let isPartySafe = false; // Hopefully, this doesn't change.
+//Question 4
+const shiftValue = 3;
+Number.isInteger(shiftValue);
